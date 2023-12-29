8e7b2ba7a27f70a106ca863875e29fecd524eb69
Axel Velez <axelvel@live.fr>
2023-12-29 13:00:14 +0800

    :zap: Improved data loading

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 import os
 import kaggle
 import tensorflow as tf
+from preprocessing import data_loading, plot_histogram
+from model import LeukemiaDetector
+from train import training
 
-import preprocessing
-
-
+DATASET_PATH = './C-NMC_Leukemia/' if os.name == 'nt' else 'C-NMC_Leukemia/'
 
 def import_dataset():
     """
@@ -16,17 +17,19 @@ def import_dataset():
     print('Import done')
 
 
-DATASET_PATH = 'C-NMC_Leukemia/'
-
 if __name__ == '__main__':
     print('Tensorflow:', tf.__version__)
     if not os.path.isdir(DATASET_PATH):
         import_dataset()
 
-    train_data0, train_data1, train_data2, val_data, test_data = preprocessing.data_loading()
-    preprocessing.plot_histogram(train_data0,"fold_0")
-    preprocessing.plot_histogram(train_data1,"fold_1")
-    preprocessing.plot_histogram(train_data2,"fold_2")
-    preprocessing.plot_histogram(val_data,"val")
-    preprocessing.plot_histogram(test_data,"test")
-    
+    # Creating dataloaders
+    train_loader, test_loader, val_loader = data_loading(dataset_path=DATASET_PATH, batch_size=16)
+
+    # Plotting class distribution
+    plot_histogram(train_loader)
+
+    # Training loop
+    INPUT_SIZE = 450 * 450 * 3
+    OUTPUT_SIZE = 2
+    model = LeukemiaDetector(INPUT_SIZE, OUTPUT_SIZE)
+    training(model, train_loader)
--- train.py
+++ train.py
@@ -1,38 +1,8 @@
-import os
 import tensorflow as tf
-from model import LeukemiaDetector
-#from sklearn.model_selection import train_test_split
 
-DATASET_PATH = 'C-NMC_Leukemia/'
+def training(model, train_loader):
 
-TRAIN_PATH = DATASET_PATH + 'training_data/'
-FOLDS = sorted(os.listdir(TRAIN_PATH))
-
-total_dataset = []
-
-for fold in FOLDS:
-    loaded_imgs = tf.keras.utils.image_dataset_from_directory(TRAIN_PATH + fold, image_size=(450, 450))
-    total_dataset.append(loaded_imgs)
-
-
-def concat_dataloader(head, rest):
-    if not rest:
-        return head
-    current = head
-    remaining = rest[1:]
-    current = current.concatenate(rest[0])
-    return concat_dataloader(current, remaining)
-
-
-loader = concat_dataloader(total_dataset[0], total_dataset[1:])
-
-INPUT_SIZE = 450 * 450 * 3
-OUTPUT_SIZE = 2
-
-model = LeukemiaDetector(INPUT_SIZE, OUTPUT_SIZE)
-
-# Training Loop
-
-for inputs, labels in loader:
-    outputs = model(inputs)
-    print(outputs.shape)
+    # Training Loop
+    for inputs, labels in train_loader:
+        print(inputs.shape)
+        outputs = model(inputs)
